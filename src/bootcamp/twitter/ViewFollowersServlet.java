c3502c696337d7ea05f6001ff51382ba1e220dd9
codeaudit <ceperez@codeaudit.com>
2016-08-05 16:11:40 -0400

    update servlet to spit out data

--- src/bootcamp/twitter/ViewFollowersServlet.java
+++ src/bootcamp/twitter/ViewFollowersServlet.java
@@ -3,6 +3,7 @@ package bootcamp.twitter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import bootcamp.twitter.orm.Follower;
+import bootcamp.twitter.orm.Tweet;
+import bootcamp.twitter.orm.User;
 
 public class ViewFollowersServlet extends HttpServlet
 {
@@ -25,14 +35,25 @@ public class ViewFollowersServlet extends HttpServlet
     	response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
         
-    	Connection c = null;
+        ConnectionSource  connectionSource;
+    	Dao<User, Integer> userDao; 
+    	Dao<Follower, Integer> followerDao = null; 
+    	Dao<Tweet, Integer> tweetDao = null;
+        
         try {
-          Class.forName("org.sqlite.JDBC");
-          c = DriverManager.getConnection("jdbc:sqlite:test.db");
-        } catch ( Exception e ) {
-          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-          System.exit(0);
-        }
+			connectionSource =
+					  new JdbcConnectionSource("jdbc:sqlite:test.db");
+			
+			userDao = 
+					  DaoManager.createDao(connectionSource, User.class);
+			followerDao = 
+					  DaoManager.createDao(connectionSource, Follower.class);
+			tweetDao = 
+					  DaoManager.createDao(connectionSource, Tweet.class);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
        
         JSONObject obj = new JSONObject();
         obj.put("message", "database opened!");
@@ -41,6 +62,17 @@ public class ViewFollowersServlet extends HttpServlet
         
         JSONArray list = new JSONArray();
         list.add(obj);
+        
+       
+        for (Follower follower: followerDao) {
+        	 JSONObject o = new JSONObject();
+        	 o.put("id", follower.getId());
+        	 o.put("user1", follower.getUser1());
+        	 o.put("user2", follower.getUser2());
+
+        	 
+        	 list.add(o);
+		}
         
         response.getWriter().print(list.toJSONString());
     }
--- src/bootcamp/twitter/ViewTweetsServlet.java
+++ src/bootcamp/twitter/ViewTweetsServlet.java
@@ -3,6 +3,7 @@ package bootcamp.twitter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import bootcamp.twitter.orm.Follower;
+import bootcamp.twitter.orm.Tweet;
+import bootcamp.twitter.orm.User;
 
 public class ViewTweetsServlet extends HttpServlet
 {
@@ -24,23 +34,51 @@ public class ViewTweetsServlet extends HttpServlet
     {
     	response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
+      
+        ConnectionSource  connectionSource;
+    	Dao<User, Integer> userDao; 
+    	Dao<Follower, Integer> followerDao; 
+    	Dao<Tweet, Integer> tweetDao = null;
         
-    	Connection c = null;
         try {
-          Class.forName("org.sqlite.JDBC");
-          c = DriverManager.getConnection("jdbc:sqlite:test.db");
-        } catch ( Exception e ) {
-          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-          System.exit(0);
-        }
-        
+			connectionSource =
+					  new JdbcConnectionSource("jdbc:sqlite:test.db");
+			
+			userDao = 
+					  DaoManager.createDao(connectionSource, User.class);
+			followerDao = 
+					  DaoManager.createDao(connectionSource, Follower.class);
+			tweetDao = 
+					  DaoManager.createDao(connectionSource, Tweet.class);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+		
+	
         JSONObject obj = new JSONObject();
         obj.put("message", "database opened!");
         obj.put("class", getClass().getName());
         obj.put("session", request.getSession(true).getId());
         
+        
         JSONArray list = new JSONArray();
         list.add(obj);
+        
+        for (Tweet tweet: tweetDao) {
+        	 JSONObject o = new JSONObject();
+        	 o.put("id", tweet.getId());
+        	 o.put("time", tweet.getHitTime());
+        	 o.put("message", tweet.getMessage());
+        	 o.put("user", tweet.getUserId());
+
+        	 
+        	 list.add(o);
+		}
+        
+        
+        
         
         response.getWriter().print(list.toJSONString());
     }
